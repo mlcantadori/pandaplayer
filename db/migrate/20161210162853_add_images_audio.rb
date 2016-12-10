3a78ea66abf05a41367d3bc4b211d277751002eb
Malu <mlcantadori@gmail.com>
2016-12-10 16:43:22 -0200

    Projeto Rails Girls - com posts

---
+++ db/migrate/20161210162853_add_images_audio.rb
@@ -0,0 +1,13 @@
+class AddImagesAudio < ActiveRecord::Migration[5.0]
+
+  def up
+    add_attachment :posts, :image
+    add_attachment :posts, :audio
+  end
+
+  def down
+    remove_attachment :posts, :image
+    remove_attachment :posts, :audio
+  end
+
+end
